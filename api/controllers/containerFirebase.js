711435f2b68664ce8f920d93afe69e7bf866f2a0
muxinio <mustiif2000@gmail.com>
2022-12-22 13:16:39 -0300

    FireBase_Clase_20

---
+++ api/controllers/containerFirebase.js
@@ -0,0 +1,50 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require("../db/festive-rarity-328123-firebase-adminsdk-12jqh-f93d7b5956.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const { getFirestore, doc, getDoc } = require("firebase-admin/firestore");
+
+class Container {
+  constructor() {
+    this.db = getFirestore();
+  }
+  
+  save(obj) {
+    try {
+      return this.db.collection("products").add(obj);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  getById(id) {
+    try {
+      const data = this.db.doc(`/products/${id}`).get();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+ 
+  getAll() {
+    try {
+      return this.model.find();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  deleteById(id) {
+    try {
+      return this.model.findByIdAndDelete(id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = Container;
